0c6efc60cb4f750bcd7d7a323f5114c5fda492ec
sx107@duke.edu <sx107@duke.edu>
2025-12-09 04:24:48 +0000

    Created Approach1.FeatureRowTemplate and Approach1_2; Edited ServerModule1, Approach1 and Approach1.RowTemplate; Deleted Approach1.RowTemplate4

--- client_code/Approach1/FeatureRowTemplate/__init__.py
+++ client_code/Approach1/FeatureRowTemplate/__init__.py
@@ -1,15 +1,15 @@
-from ._anvil_designer import RowTemplateTemplate
+from ._anvil_designer import FeatureRowTemplateTemplate
 from anvil import *
-import anvil.server
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
 import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+import anvil.server
 
 
-class RowTemplate(RowTemplateTemplate):
+class FeatureRowTemplate(FeatureRowTemplateTemplate):
   def __init__(self, **properties):
     self.init_components(**properties)
 
--- client_code/Approach1_2/__init__.py
+++ client_code/Approach1_2/__init__.py
@@ -1,15 +1,15 @@
-from ._anvil_designer import RowTemplate4Template
+from ._anvil_designer import Approach1_2Template
 from anvil import *
+import anvil.server
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
 import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-import anvil.server
 
 
-class RowTemplate4(RowTemplate4Template):
+class Approach1_2(Approach1_2Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
